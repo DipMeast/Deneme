6884f9a0106699315d36237873d7fc849c07debf
DipMeast <76068560+DipMeast@users.noreply.github.com>
2021-04-30 16:28:47 +0300

    Update tst.sh

--- tst.sh
+++ tst.sh
@@ -2,13 +2,7 @@
 
 
 start(){
-	clear
-	echo -e "\e[33m▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄  \e[31mCode: \e[37mYaman Efkar\n\e[33m▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄  \e[31mİnstagram: \e[37myamanefkarr\n\e[33m▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄  \e[31mYoutube: \e[37mYaman Efkar\n\e[33m▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄  \e[31mGitHub: \e[37myamanefkar\n\e[33m▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄  Tool Name: Wordlist-Pluas\n\n\n"
 
+	echo "Selam"
+}
 
-
-	
-echo "Bu Tool DipMeast Tarafından Kodlandı...." >> ok.txt
-cd ..
-bash tst.sh	
-fi
