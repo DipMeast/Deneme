286d2e005c8296e28fd5fc40423007801e869fdf
DipMeast <76068560+DipMeast@users.noreply.github.com>
2021-04-30 17:59:51 +0300

    Update tst.sh

--- tst.sh
+++ tst.sh
@@ -1,16 +1,39 @@
 #!/bin/bash
+{
 
 clear
 
+}
+{
 echo \+---------------------------+/
 echo   ........Dipmeast...........
 echo \+---------------------------+/
+
+}
 
 spin () {
 
 local pid=$!
 local delay=0.25
 local spinner=( '█■■■■' '■█■■■' '■■█■■' '■■■█■' '■■■■█' )
+
+while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+
+for i in "${spinner[@]}"
+do
+	tput civis
+	echo -ne "\033[34m\r[*] Downloading..please wait.........\e[33m[\033[32m$i\033[33m]\033[0m   ";
+	sleep $delay
+	printf "\b\b\b\b\b\b\b\b";
+done
+done
+printf "   \b\b\b\b\b"
+tput cnorm
+printf "\e[1;33m [Done]\e[0m";
+echo "";
+
+}
+
 
 
 echo " instagram Adresime Yönlendiriliyorsunuz...! "
